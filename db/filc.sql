412e9de5b46f40e93d24aee0babfa986b76262f9
Dome-Zoltan <dome.zoltan-2021@keri.mako.hu>
2026-05-06 16:32:12 +0200

    Add testing data to database.

--- db/filc.sql
+++ db/filc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 26, 2026 at 10:36 AM
+-- Generation Time: May 06, 2026 at 04:29 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -39,9 +39,8 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `user_id`, `post_id`, `content`) VALUES
-(1, 7, 14, 'asd'),
-(2, 7, 14, 'komment!'),
-(3, 7, 14, 'a');
+(1, 2, 1, 'Jó napot Tanár Úr!'),
+(2, 3, 1, ':)');
 
 -- --------------------------------------------------------
 
@@ -59,14 +58,7 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`id`, `name`) VALUES
-(1, 'Math 101'),
-(2, 'Physics 101'),
-(3, 'History 101'),
-(4, 'Furulya 4'),
-(5, 'Computer Science 101'),
-(8, 'krzus'),
-(10, 'asd'),
-(12, 'aha aha aha');
+(1, 'Matematika');
 
 -- --------------------------------------------------------
 
@@ -78,19 +70,17 @@ CREATE TABLE `grades` (
   `id` int(11) NOT NULL,
   `course_id` int(11) NOT NULL,
   `weight` double NOT NULL DEFAULT 1,
-  `name` varchar(100) DEFAULT NULL
+  `name` varchar(100) DEFAULT NULL,
+  `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 --
 -- Dumping data for table `grades`
 --
 
-INSERT INTO `grades` (`id`, `course_id`, `weight`, `name`) VALUES
-(1, 1, 1, 'Homework'),
-(2, 1, 2, 'Midterm'),
-(3, 1, 3, 'Final'),
-(4, 2, 1, 'Lab'),
-(5, 3, 1, 'Essay');
+INSERT INTO `grades` (`id`, `course_id`, `weight`, `name`, `date`) VALUES
+(1, 1, 1, 'Hatványozás', '2026-05-04 10:00:00'),
+(2, 1, 2, 'Témazáró', '2026-04-16 16:25:18');
 
 -- --------------------------------------------------------
 
@@ -102,14 +92,6 @@ CREATE TABLE `join_codes` (
   `course_id` int(11) NOT NULL,
   `code` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
-
---
--- Dumping data for table `join_codes`
---
-
-INSERT INTO `join_codes` (`course_id`, `code`) VALUES
-(8, 'aaaaaaaa'),
-(2, 'bbbbbbbb');
 
 -- --------------------------------------------------------
 
@@ -131,7 +113,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `type_id`, `course_id`, `user_id`, `posted_at`, `content`) VALUES
-(14, 1, 12, 7, '2026-03-25 05:16:39', '\"asdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk\\n\\n\\nNamsfdglkfdmlkdmhlkgfmnh\\n\\n\\nfghjéfglhkélghfjkpél\"');
+(1, 1, 1, 1, '2026-05-06 16:19:24', '\"Sziasztok!\"');
 
 -- --------------------------------------------------------
 
@@ -181,18 +163,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `expires_at`) VALUES
-('1PwwMzqShUPggCnCEB9mGkkF4KQa8EoIw/DTo51z/mU=', 7, '2026-04-09 10:58:06'),
-('2/73v++KDcYdC8F8+WwndajYgIQjwhQaU/JZ20z2WDg=', 7, '2026-04-09 10:58:07'),
-('2WfQxvMgNEpfC9oFJnjXuAEAJDU89+y+7uejbGTMpMU=', 7, '2026-04-09 10:58:07'),
-('BPsSVbMhr6hQ4cM4ml/TRsfoWsVrT9vLbESERkp13KY=', 7, '2026-04-09 10:58:12'),
-('gJ1XiDLlc7x/Uw2DR7gmenHW2HFPUIi49JiqDgW9h9Y=', 7, '2026-04-19 16:25:32'),
-('oNs7gDJ9m3vguQePLU5NPPMnnSUx0W13AyiBBwI8bnU=', 7, '2026-04-09 10:58:12'),
-('OSwgkAeXqWyyehtP87R7SdsqcAuVcrKP7gn5Au2f558=', 7, '2026-04-09 10:58:00'),
-('qvzHhovnoGtXwxvWvdkmVKzVYPkScXpl6ap9HPh8dHM=', 7, '2026-04-09 10:58:11'),
-('tafUaTkv/pVsuaF6tXoNEzP7F4uMWU9+INUJA5KrOP0=', 7, '2026-04-09 10:58:08'),
-('UAtkBNwwZXsLiCcMSWz2BVRPPLQOSPeqoqm0GCWNNjg=', 6, '2026-03-14 15:28:06'),
-('WdmYvSoXaff/827ozGq8Gtstml5VJBA92RShjSF5Pjs=', 7, '2026-03-27 13:07:23'),
-('wk9h2XeHBccCVt3SlYEtvYIf19Ctjk5N4uC6CoXDiy8=', 7, '2026-04-09 10:58:12');
+('VnujueZVIXhgLhOzCQ/tsh6wInP9qywCCaHdco020eA=', 1, '2026-06-05 16:22:44');
 
 -- --------------------------------------------------------
 
@@ -212,10 +183,9 @@ CREATE TABLE `student_grades` (
 
 INSERT INTO `student_grades` (`student_id`, `grade_id`, `grade`) VALUES
 (2, 1, 4),
-(2, 2, 5),
-(3, 1, 3),
-(4, 4, 4),
-(3, 5, NULL);
+(2, 2, 2),
+(3, 1, 4),
+(3, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -236,14 +206,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `hash`, `salt`) VALUES
-(1, 'Alice Teacher', 'alice@school.com', 'hash1', 'salt1'),
-(2, 'Bob Student', 'bob@student.com', 'hash2', 'salt2'),
-(3, 'Carol Student', 'carol@student.com', 'hash3', 'salt3'),
-(4, 'Dave Student', 'dave@student.com', 'hash4', 'salt4'),
-(5, 'Eve Teacher', 'eve@school.com', 'hash5', 'salt5'),
-(6, 'Név Neves', 'neves@nev.com', 'lJiIobcBnYeDF2Mmgylgd4/cxksGeeP/eNvHc8y+F7X53amH+IIZ4ZrZNJWE6K/El8ScVb6l+IdQIOePgMoDO1RtmHhfhmCJL/w8DfVN3+rs5imCNNwAygVsh/XiBrL1VvZANv47WhsfGn3TjzPJ3/4X7TIdBqipiQF+0SiDzYE=', 'J48nI18EYZEc0pH7UoVZTA=='),
-(7, 'asd', 'aha@aha.aha', '/IlXy+iM1trI4TLS3MCf86ffQSGMkzWxAYBvKENUpDMeaCd8Ck1thtFRKAnKwcfxba1n0/uBZMEhq5Z8K1SsTeN36+IWBJ4uf+HQQ7BDpVnsHsOfklDcJB0h2VakhDgf13FtLzVnka9K2EM9UGLcOxMLPuc0fQ5wiPfGUHIIpFI=', '7MwVvNlglgWUb+OsZ0Wpiw=='),
-(8, 'asd', 'asd2@asd.com', 'CNSJ+q33M/EFDUVucY+x2joSs3Uh3au128owG/+uScgKnhvKg9QNIueL9VQumooLYRlmtyykgX4WipnovGD1s5UY4udciEQp7FV1Tgh1Dcc9HxhX96Ie+VGwC8fuEmi5L/RPtijfRopM8X1wWQq/MaK6KfGL7v1r7Tsj0KAhR88=', 'UqosOIuQwOmPQyBKPQ/3+Q==');
+(1, 'Kovács Lajos', 'kovacslajos@gmail.com', 'pRh7g14P65hZBI1hDcqtq2lV0grOo5CurHzl7Jno26RJsBH8sd1UeISEJE4XVcYQTKQnt+2xT5TFGMUT1mw8QzsJ4oBQIefpinMimYZ+NvoEuji/tz/TOOjHIOd4U5LsOvUDQizCcjWzNjvXXTop+Ck60sTlD/sh9CWT96Uzdb4=', 'UDH9ZmFGZoUbBveNuJklHQ=='),
+(2, 'Kis Gergő', 'kisgergo@gmail.com', 'YpOH13E2Xg+Z+ZxVXC+H66yhyrKhq5UK5K2KGlLmqVq1NLAx84IDA4TXSQ50vEQn2hZQcAz9t1ISIfy7in4xhELzF4TfY+qhab7MqZrMBk62oV23YOX8qwTadbtoudEFQGI3xNCQMKPt2Fdk8/VBW2fM8hbQ/wBlnEoRnAmKd/k=', 'NLP1lL3hbRRhh6IFJ3unXQ=='),
+(3, 'Sándor Péter', 'sandorpeter@gmail.com', 'Mx1r9NcWoNR1ajg2tEUazPgkxhuvt1f+MubMesdxvdcItEsqaxvHE0NvLF03Bl77PWh5klSXNmdtRLTe2cIFOhHWiIaq4aGcDIvxbhU07VkmzxFYEq03SI+FlzsKVp0VPAT3GLDEGJ9o7Tvi2pxMezZi86vIP2MJIUaTxCQ1t3g=', 'uKFmA9vRUfxmDAww+eIcHQ==');
 
 -- --------------------------------------------------------
 
@@ -264,13 +229,7 @@ CREATE TABLE `user_courses` (
 INSERT INTO `user_courses` (`user_id`, `course_id`, `role_id`) VALUES
 (2, 1, 1),
 (3, 1, 1),
-(4, 2, 1),
-(1, 1, 2),
-(5, 2, 2),
-(5, 3, 2),
-(7, 8, 2),
-(7, 10, 2),
-(7, 12, 2);
+(1, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -389,25 +348,25 @@ ALTER TABLE `user_roles`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `grades`
 --
 ALTER TABLE `grades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `post_types`
@@ -419,7 +378,7 @@ ALTER TABLE `post_types`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user_roles`
@@ -430,6 +389,12 @@ ALTER TABLE `user_roles`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `grades`
@@ -443,6 +408,13 @@ ALTER TABLE `grades`
 ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `post_types` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `post_completed`
+--
+ALTER TABLE `post_completed`
+  ADD CONSTRAINT `post_completed_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  ADD CONSTRAINT `post_completed_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `sessions`
